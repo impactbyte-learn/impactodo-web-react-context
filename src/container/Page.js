ae03f028c02280dfd8a748c3b0d8f409a6e56647
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-25 07:19:29 +0700

    Refactor Home to be stateless and separate TodoApp into its own

--- src/container/Page.js
+++ src/container/Page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import { Container, Row, Col } from "reactstrap";
 
 import NavigationBar from "../medium/NavigationBar";
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,87 +1,13 @@
 import React from "react";
-import * as axios from "axios";
 
 import Page from "../container/Page";
 
-import InputText from "../small/InputText";
-import Button from "../small/Button";
-import TodoList from "../small/TodoList";
-import Todo from "../small/Todo";
+import TodoApp from "../large/TodoApp";
 
-const API_URL = process.env.REACT_APP_API_URL || `http://localhost:3000`;
-
-class Home extends React.Component {
-  state = {
-    text: "",
-    todos: []
-  };
-
-  constructor() {
-    super();
-    this.findAll = this.findAll.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentWillMount() {
-    this.findAll();
-  }
-
-  async findAll() {
-    axios.get(`${API_URL}/todos`).then(res => {
-      this.setState({ todos: res.data });
-    });
-  }
-
-  async handleChange(event) {
-    const text = await event.target.value;
-    if (text !== "") this.setState({ text: text });
-  }
-
-  async handleSubmit(event) {
-    event.preventDefault();
-    const text = await this.state.text;
-    if (text !== "") {
-      axios
-        .post(`${API_URL}/todos`, {
-          text: text
-        })
-        .then(res => {
-          this.findAll();
-        });
-    }
-  }
-
-  handleDelete(event) {
-    const id = "";
-    axios.delete(`${API_URL}/todos/${id}`).then(res => {
-      this.findAll();
-    });
-  }
-
-  render() {
-    const todos = this.state.todos;
-
-    return (
-      <Page title="Impact Todo">
-        <form onSubmit={this.handleSubmit}>
-          <InputText
-            type="text"
-            placeholder="New todo text"
-            onChange={this.handleChange}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-
-        <TodoList>
-          {todos &&
-            todos.map(todo => {
-              return <Todo key={todo.id}>{todo.text}</Todo>;
-            })}
-        </TodoList>
-      </Page>
-    );
-  }
-}
+const Home = () => (
+  <Page title="Impact Todo">
+    <TodoApp />
+  </Page>
+);
 
 export default Home;
