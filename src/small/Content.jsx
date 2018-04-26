6cf428b0f7b9762b844a84197629105e633ee5c6
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-27 01:18:42 +0700

    Use Jest and prop-types

--- src/small/Content.jsx
+++ src/small/Content.jsx
@@ -1,6 +1,14 @@
 // @flow
+
+export type Color = {
+  a: number,
+  b: number,
+  g: number,
+  r: number
+};
+
 function foo(x: ?number): string {
-  if (x) return x;
+  if (x) return x.toString();
 
   return "default string";
 }
